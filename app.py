37eb82f4cd65ba39d0b91f6752d32229a9398781
Mallu03 <mallappwaghamore@gmail.com>
2026-07-04 19:52:00 +0530

    Added PDF validation, Upload filename and upload time

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER="uploads"
+app.config["UPLOAD_FOLDER"]=UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return filename.endswith(".pdf")
+
 @app.route("/")
 def home():
-    return "Welcome to Resume Anlyzer"
+    return render_template("index.html")
+
+@app.route("/upload",methods=["POST"])
+def upload():
+    file=request.files["resume"]
+
+    if file and allowed_file(file.filename):
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"],file.filename))
+        upload_time=datetime.now().strftime("%d-%m-%Y %I:%M %p")
+        return render_template("result.html",filename=file.filename,upload_time=upload_time)
+    return "Only PDF files are allowed!"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
