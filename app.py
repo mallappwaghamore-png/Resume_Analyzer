daca88b014d2a4cfb29d755435565b41c1f0057a
Mallu03 <mallappwaghamore@gmail.com>
2026-07-10 23:38:51 +0530

    Added MongoDB integration and stored resume details

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,request
 import os
 from datetime import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client=MongoClient("mongodb://localhost:27017/")
+db=client["resume_analyzer"]
+collection=db["uploads"]
 
 UPLOAD_FOLDER="uploads"
 app.config["UPLOAD_FOLDER"]=UPLOAD_FOLDER
@@ -21,6 +25,11 @@ def upload():
     if file and allowed_file(file.filename):
         file.save(os.path.join(app.config["UPLOAD_FOLDER"],file.filename))
         upload_time=datetime.now().strftime("%d-%m-%Y %I:%M %p")
+
+        document = {"filename":file.filename, "upload_time":upload_time, "status": "Success"}
+
+        collection.insert_one(document)
+
         return render_template("result.html",filename=file.filename,upload_time=upload_time)
     return "Only PDF files are allowed!"
 
